71bc9f67425c6d76f3d861c1631a5c8d76300f02
sadohu <sadohu26@yahoo.com>
2023-11-23 20:17:37 -0500

    init project

---
+++ src/main/java/pe/edu/cibertec/Tema2Sesion1/Tema2Sesion1Application.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.Tema2Sesion1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tema2Sesion1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tema2Sesion1Application.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/Tema2Sesion1/Tema2Sesion1ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.Tema2Sesion1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tema2Sesion1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
